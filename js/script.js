777f0d7e37862839889a37ce704f10b4d9a6e17a
Murilo Pita <murilo.junior95@hotmail.com>
2023-05-26 17:10:34 -0300

    Ativando os itens do orçamento através dos parâmetros da URL

--- js/script.js
+++ js/script.js
@@ -16,3 +16,23 @@ function ativarLink(link) {
 }
 
 links.forEach(ativarLink);
+
+
+//Ativar Itens do Orçamento
+
+const parametros = new URLSearchParams(location.search); //Retorna uma Array com os paramentros de busca.
+
+//Função que mostra cada parametro dentro da array.
+function ativarProduto(parametro) {
+    //Selecionando o elemento pelo ID.
+    const elemento = document.getElementById(parametro);
+    //Verificar se os parametros existem, mesmo que tenham outros.
+    if (elemento) {
+        elemento.checked = true; //Marcar o box com o tipo de seguro selecionado.
+    }
+
+}
+
+parametros.forEach(ativarProduto);
+
+
